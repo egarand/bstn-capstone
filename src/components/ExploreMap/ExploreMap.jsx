663cb5c55f21b65a7485aba667c7228599a2498b
Erica Garand <egarand13@outlook.com>
2024-10-17 14:29:13 -0400

    add ExploreMap.PoiOverlay subcomponent

--- src/components/ExploreMap/ExploreMap.jsx
+++ src/components/ExploreMap/ExploreMap.jsx
@@ -1,6 +1,6 @@
-import { MapContainer, Polyline, Popup, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Polygon, Polyline, Popup, TileLayer, useMap } from "react-leaflet";
 import "./ExploreMap.scss";
-import { useEffect } from "react";
+import { Children, useEffect } from "react";
 
 const algonquinCoords = [45.8372, -78.3791];
 const tileAttribution =
@@ -8,8 +8,29 @@ const tileAttribution =
 &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> \
 &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`;
 
+const pathOptions = {
+	trail: {
+		weight: 8,
+		color: "purple"
+	},
+	campground: {
+		color: "red"
+	},
+	reserve: {
+		color: "blue"
+	}
+}
 
-function ExploreMap() {
+function ExploreMap({ children }) {
+	const subcomponentNames = Object.keys(ExploreMap);
+	const subcomponents =
+		subcomponentNames.map(
+			(key) => Children.map(
+				children,
+				(child) => child.type.name === key ? child : null
+			)
+		);
+
 	return (
 		<MapContainer center={algonquinCoords} zoom={10} scrollWheelZoom={false} className="explore-map">
 			<TileLayer
@@ -19,7 +40,7 @@ function ExploreMap() {
 				url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
 			/>
 
-			<CenterOnUserOnMount/>
+			{subcomponents.map((sc) => sc)}
 
 		</MapContainer>
 	);
@@ -34,5 +55,23 @@ function CenterOnUserOnMount() {
 	}, [map]);
 	return <></>;
 }
+ExploreMap.CenterOnUserOnMount = CenterOnUserOnMount;
+
+function PoiOverlay({ pois }) {
+	return (<>
+	{pois.map((p) => {
+		const key = `${p.osm_type}${p.osm_id}`;
+		const TagName = p.category === "trail"
+						? Polyline
+						: Polygon;
+		return (
+		<TagName key={key} pathOptions={pathOptions[p.category]} positions={p.geometry}>
+			<Popup>Test Popup for {p.tags.name}</Popup>
+		</TagName>
+		);
+	})}
+	</>);
+}
+ExploreMap.PoiOverlay = PoiOverlay;
 
 export default ExploreMap;
--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import ExploreMap from "../../components/ExploreMap/ExploreMap";
 import "./ExplorePage.scss";
+import axios from "axios";
 
 function ExplorePage() {
+	const [pois, setPois] = useState([]);
+
+	function handleSubmit() {
+		axios.get("http://localhost:5050/pois")
+			.then(({ data }) => setPois(data));
+	};
+
 	return (
 		<div>
 			<h1>ExplorePage</h1>
 
-			<ExploreMap/>
+			<button onClick={handleSubmit}>Get Data</button>
+
+			<ExploreMap>
+				<ExploreMap.CenterOnUserOnMount/>
+				<ExploreMap.PoiOverlay pois={pois}/>
+			</ExploreMap>
 
 		</div>
 	);
