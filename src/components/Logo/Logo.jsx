28b64edc3b31851ff40bcf9e78fa3b9e6252d017
Erica Garand <egarand13@outlook.com>
2024-10-19 03:18:11 -0400

    add Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+import "./Logo.scss";
+
+/** The Hike & Seek logo, colorizable with CSS. By default, inherits the `color`
+ * of its container. Pass a `className` to control the width and set the `color`. */
+function Logo({ className = "", alt = "Hike & Seek" }) {
+	return (
+		<div
+			className={`logo ${className}`}
+			role="img"
+			aria-label={alt}
+		>{alt}</div>
+	);
+}
+
+export default Logo
