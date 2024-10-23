8c911e0d6b87a65ca4556f3bca1b2099bd2195a1
Erica Garand <egarand13@outlook.com>
2024-10-23 11:14:46 -0400

    add basic structure to SpeciesDetailPage

--- src/pages/SpeciesDetailPage/SpeciesDetailPage.jsx
+++ src/pages/SpeciesDetailPage/SpeciesDetailPage.jsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useIsMounted from "../../hooks/useIsMounted";
+import api from "../../utils/api";
 import "./SpeciesDetailPage.scss";
 
 function SpeciesDetailPage() {
-	return (
-		<div>SpeciesDetailPage</div>
-	);
+	const { inat_id } = useParams();
+	const isMounted = useIsMounted();
+
+	const [species, setSpecies] = useState(null);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const { data: speciesData } = await api("get", `/life/${inat_id}`);
+				if (!isMounted.current) { return; }
+				setSpecies(() => speciesData);
+			} catch {
+				// do something
+			}
+		})();
+	}, [inat_id, isMounted]);
+
+	return (<>
+		<h1>{species?.common_name}</h1>
+		<p>{species?.scientific_name}</p>
+		<figure>
+			<img alt="" src={species?.photo.medium_url}/>
+			<figcaption>
+				{species?.photo.attribution.replace("(c)", "©")}
+			</figcaption>
+		</figure>
+		<p>
+			{species?.wikipedia_excerpt}
+		</p>
+		<p><a target="blank" href={species?.wikipedia_url}>
+			Read more on Wikipedia
+		</a></p>
+
+		<pre style={{wordWrap: "break-word", wordBreak: "break-word", whiteSpace: "break-spaces", maxWidth: "100%"}}>{JSON.stringify(species,null,2)}</pre>
+	</>);
 }
 
 export default SpeciesDetailPage;
