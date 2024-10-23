70a02e8af5224c71b5384335ca74f82523a529b1
Erica Garand <egarand13@outlook.com>
2024-10-23 15:02:09 -0400

    parse & format location detail tags

--- src/pages/LocationDetailPage/LocationDetailPage.jsx
+++ src/pages/LocationDetailPage/LocationDetailPage.jsx
@@ -3,14 +3,23 @@ import { useLocation, useParams } from "react-router-dom";
 import useIsMounted from "../../hooks/useIsMounted";
 import { polygonCentroid, trycatch } from "../../utils";
 import api from "../../utils/api";
+
 import { AnnouncedLink } from "../../navigation-accessibility";
 import ExploreMap from "../../components/ExploreMap/ExploreMap";
 import DocTitle from "../../components/DocTitle/DocTitle";
 import SpeciesLink from "../../components/SpeciesLink/SpeciesLink";
 import Pagination from "../../components/Pagination/Pagination";
+import Icon from "../../components/Icon/Icon";
+
 import "./LocationDetailPage.scss";
+import linkSrc from "../../assets/icons/link.svg";
+import moneySrc from "../../assets/icons/money.svg";
+import pawprintSrc from "../../assets/icons/pawprint.svg";
+import wheelchairSrc from "../../assets/icons/wheelchair.svg";
 
 const perPage = 16;
+const primaryListTags = ["website", "wheelchair", "dog", "fee"];
+const primaryTags = ["name", "description", ...primaryListTags];
 
 function LocationDetailPage() {
 	const { state } = useLocation();
@@ -68,9 +77,7 @@ function LocationDetailPage() {
 				if (!isMounted.current) { return; }
 				setSpecies(() => lifeData);
 				setLoadingSpecies(() => false);
-			} catch (error) {
-				console.error(error);
-
+			} catch {
 				// do something
 			}
 		})();
@@ -93,12 +100,84 @@ function LocationDetailPage() {
 				<ExploreMap.CenterOnCoordinate latlon={centroid}/>
 				<ExploreMap.Poi poi={poi} noPopup />
 			</ExploreMap>
-			<dl>
-				{poi.tags.description && <p>poi.tags.description</p>}
+			{poi.tags.description && (
+				<p className="location-page__description">
+					{poi.tags.description}
+				</p>
+			)}
+			<dl className="location-page__tag-list">
+				{poi.tags.website && (<div className="location-page__tag">
+					<dt className="location-page__tag-title">
+						<Icon className="location-page__tag-icon" src={linkSrc} alt=""/>
+						Website
+					</dt>
+					<dd className="location-page__tag-content">
+						<a className="location-page__link" target="blank" href={poi.tags.website}>Go to External Site</a>
+					</dd>
+				</div>)}
+				<div className="location-page__tag">
+					<dt className="location-page__tag-title">
+						<Icon className="location-page__tag-icon" src={moneySrc} alt=""/>
+						Entry Fee?
+					</dt>
+					<dd>{(()=>{switch (poi.tags.fee) {
+						case "yes":
+							return "Yes";
+						case "no":
+							return "No";
+						default:
+							return "Unknown";
+					}})()}</dd>
+				</div>
+				<div className="location-page__tag">
+					<dt className="location-page__tag-title">
+						<Icon className="location-page__tag-icon" src={wheelchairSrc} alt=""/>
+						Wheelchair Accessible?
+					</dt>
+					<dd>{(()=>{switch (poi.tags.wheelchair) {
+						case "yes":
+							return "Yes; fully accessible";
+						case "limited":
+							return "Yes; partial access";
+						case "no":
+							return "No";
+						default:
+							return "Unknown";
+					}})()}</dd>
+				</div>
+				<div className="location-page__tag">
+					<dt className="location-page__tag-title">
+						<Icon className="location-page__tag-icon" src={pawprintSrc} alt=""/>
+						Dogs Allowed?
+					</dt>
+					<dd>{(()=>{switch (poi.tags.dog) {
+						case "yes":
+						case "unleashed":
+							return "Yes; follow local leashing laws";
+						case "leashed":
+							return "Yes; must be leashed";
+						case "outside":
+							return "Only in outside areas";
+						case "no":
+							return "No";
+						default:
+							return "Unknown; most likely yes, but check";
+					}})()}</dd>
+				</div>
 			</dl>
-		<pre style={{whiteSpace: "pre-line", wordWrap: "break-word", wordBreak: "break-word"}}>{JSON.stringify(poi?.tags,null,2)}</pre>
-			<h2>Other Tags</h2>
 
+			{Object.entries(poi.tags).filter(([t]) => !primaryTags.includes(t)).length && (
+				<details className="location-page__tag-list-collapsible-wrapper">
+				<summary className="location-page__tag-list-collapsible-header">Other Tags</summary>
+				<dl className="location-page__tag-list">
+					{Object.entries(poi.tags).filter(([t]) => !primaryTags.includes(t)).map(([t,v]) => (
+					<div key={t} className="location-page__tag">
+						<dt className="location-page__tag-title">{t}</dt>
+						<dd className="location-page__tag-content">{v}</dd>
+					</div>))}
+				</dl>
+				</details>
+			)}
 		</>)}
 		</section>
 
