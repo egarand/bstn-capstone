d7dc69c0c96609c2b79db7211077c00ac7cb423d
Erica Garand <egarand13@outlook.com>
2024-10-23 21:44:02 -0400

    update species detail page structure

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,16 +3,18 @@ import { AnnouncedLink } from "../../navigation-accessibility";
 
 /** A CTA button. By default, renders as a `<button>`. If you provide a `to`
  * prop with a route, it will be rendered as an `<AnnouncedLink>` element that
- * goes to that route.
+ * goes to that route. If you provide a `href` prop with a URL, it will be rendered as an `<a>` with `target="blank"` that links to that external page.
  * - The `variant` prop controls the style of the button. `"primary"` (default) renders an orange button; `"secondary"` makes it white.
  * - You can provide a `className` prop to further customize styling. */
-function Button({ to = null, onClick, variant = "primary", className = "", children, ...rest }) {
-	const TagName = to ? AnnouncedLink : "button";
+function Button({ onClick, to = null, href = null, variant = "primary", className = "", children, ...rest }) {
+	const TagName = to ? AnnouncedLink : href ? "a" : "button";
 	const variantClass = variant ? `cta-button--${variant}` : "";
 	return (
 		<TagName
 			{...rest}
 			to={to}
+			href={href}
+			target={href ? "blank" : null}
 			onClick={onClick}
 			className={`cta-button ${variantClass} ${className}`}
 		>
--- src/pages/SpeciesDetailPage/SpeciesDetailPage.jsx
+++ src/pages/SpeciesDetailPage/SpeciesDetailPage.jsx
@@ -2,9 +2,11 @@ import { createElement, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useIsMounted from "../../hooks/useIsMounted";
 import api from "../../utils/api";
-import "./SpeciesDetailPage.scss";
 import ExploreMap from "../../components/ExploreMap/ExploreMap";
 import { TileLayer } from "react-leaflet";
+import DocTitle from "../../components/DocTitle/DocTitle";
+import Button from "../../components/Button/Button";
+import "./SpeciesDetailPage.scss";
 
 function SpeciesDetailPage() {
 	const { inat_id } = useParams();
@@ -17,9 +19,6 @@ function SpeciesDetailPage() {
 			try {
 				const { data: speciesData } = await api("get", `/life/${inat_id}`);
 				if (!isMounted.current) { return; }
-
-				speciesData.wikipedia_excerpt =
-					wikiExcerptToJSX(speciesData.wikipedia_excerpt);
 				setSpecies(() => speciesData);
 			} catch {
 				// do something
@@ -27,49 +26,62 @@ function SpeciesDetailPage() {
 		})();
 	}, [inat_id, isMounted]);
 
-	return (<>
-		<h1>{species?.common_name}</h1>
-		<p>{species?.scientific_name}</p>
-		<figure>
-			<img alt="" src={species?.photo.medium_url}/>
-			<figcaption>
+	return (
+	<section className="species-page" aria-busy={!species}>
+		<DocTitle title={species?.common_name}/>
+		<div className="species-page__title-wrapper">
+			<h1 className="species-page__title">{species?.common_name}</h1>
+			<p className="species-page__subtitle">{species?.scientific_name}</p>
+		</div>
+
+		<figure className="species-page__photo">
+			<img className="species-page__photo-img"
+				alt={`A ${species?.common_name}`}
+				src={species?.photo.medium_url}/>
+			<figcaption className="species-page__photo-caption">
 				{species?.photo.attribution.replace("(c)", "©")}
 			</figcaption>
 		</figure>
-		<ExploreMap className="species-page__range-map" zoom={3} minz={2} maxz={5}>
-			<ExploreMap.CenterOnUserOnMount/>
-			<TileLayer
-				minZoom={2}
-				maxZoom={5}
-				attribution='&copy; <a target="blank" href="https://inaturalist.org/">iNaturalist</a> contributers'
-				url={`https://api.inaturalist.org/v1/grid/{z}/{x}/{y}.png?ttl=${86_400}&verifiable=true&taxon_id=${species?.id}`}
-				updateInterval={2000}
-				keepBuffer={8}/>
-				{/* Most of these settings are intended to minimize requests to iNat's API. */}
-		</ExploreMap>
-		<p>
-			{species?.wikipedia_excerpt}
+		<figure className="species-page__range">
+			<ExploreMap className="species-page__range-map" zoom={3} minz={2} maxz={6}>
+				<ExploreMap.CenterOnUserOnMount/>
+				<TileLayer
+					attribution='&copy; <a target="blank" href="https://inaturalist.org/">iNaturalist</a> contributers'
+					url={`https://api.inaturalist.org/v1/grid/{z}/{x}/{y}.png?ttl=${86_400}&verifiable=true&taxon_id=${species?.id}`}
+					minZoom={2}
+					maxZoom={6}
+					updateInterval={2000}
+					keepBuffer={8}/>
+					{/* Settings intended to minimize requests to iNat's API. */}
+			</ExploreMap>
+			<figcaption className="species-page__range-caption">
+				Places the {species?.common_name} has been observed
+			</figcaption>
+		</figure>
+		<p className="species-page__wiki-excerpt">
+			<WikiExcerpt wiki={species?.wikipedia_excerpt}/>
 		</p>
-		<p><a target="blank" href={species?.wikipedia_url}>
+		<Button className="species-page__wiki-link" href={species?.wikipedia_url}>
 			Read more on Wikipedia
-		</a></p>
-
-		<pre style={{wordWrap: "break-word", wordBreak: "break-word", whiteSpace: "break-spaces", maxWidth: "100%"}}>{JSON.stringify(species,null,2)}</pre>
-	</>);
+		</Button>
+	</section>);
 }
 
 export default SpeciesDetailPage;
 
 /** Parses HTML-string wikipedia excerpts into JSX, keeping the formatting of
- * the <b> and <i> tags.
+ * the `<b>` and `<i>` tags.
  *
  * A library would be overkill since its fairly simple and only done here,
  * and other options could leave XSS loopholes. */
-function wikiExcerptToJSX(wiki) {
+function WikiExcerpt({ wiki }) {
+	if (!wiki) { return null; }
 	const parser = new DOMParser();
 	let wikiTree = parser.parseFromString(wiki, "text/html");
+	let count = 0;
 
-	/** @param {HTMLElement} el */
+	/** Traverse an HTML tree and convert it to JSX; only `<b>` and
+	 * `<i>` tags are kept, other tags are removed. */
 	function createJSX(el) {
 		if (el.nodeType === Node.TEXT_NODE || el.childNodes.length === 0) {
 			return el.textContent;
@@ -79,12 +91,16 @@ function wikiExcerptToJSX(wiki) {
 		for (const child of el.childNodes) {
 			childJSX.push(createJSX(child));
 		}
-		if (["B", "I", "P"].includes(el.tagName)) {
-			return createElement(el.tagName.toLowerCase(), null, ...childJSX);
+
+		if (["B", "I"].includes(el.tagName)) {
+			return createElement(
+				el.tagName.toLowerCase(),
+				{ key: count++ },
+				...childJSX
+			);
 		}
 		return <>{childJSX}</>;
 	}
 
-	const jsx = createJSX(wikiTree.body.firstChild);
-	return jsx;
+	return createJSX(wikiTree.body);
 }
