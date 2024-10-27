37801ab9cd4bcc8a51b77721dbaa66ed0d3a9cad
Erica Garand <egarand13@outlook.com>
2024-10-27 13:16:21 -0400

    add delete functionality to BookmarksPage

--- src/pages/BookmarksPage/BookmarksPage.jsx
+++ src/pages/BookmarksPage/BookmarksPage.jsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
+import { useAccessibleNav } from "../../navigation-accessibility";
 import { trycatch } from "../../utils";
-import useApi from "../../utils/api";
+import useApi, { api } from "../../utils/api";
+
 import Loader from "../../components/Loader/Loader";
-import PoiOverview from "../../components/PoiOverview/PoiOverview";
 import PaginatedCards from "../../components/PaginatedCards/PaginatedCards";
+import PoiOverview from "../../components/PoiOverview/PoiOverview";
+import Icon from "../../components/Icon/Icon";
+
 import "./BookmarksPage.scss";
+import closeSrc from "../../assets/icons/close.svg";
 
 function BookmarksPage() {
+	const { refocusPageTop } = useAccessibleNav();
 	const [fetchBookmarks, bookmarks, loading, error] = useApi([]);
 
 	const [taxa] = useState(
@@ -20,19 +26,31 @@ function BookmarksPage() {
 		fetchBookmarks("get", "/users/pois");
 	}, [fetchBookmarks]);
 
+	async function deleteBookmark(id) {
+		await api("delete", `/users/pois/${id}`);
+		await fetchBookmarks("get", "/users/pois");
+		refocusPageTop();
+	}
+
 	return (
 	<section className="bookmark-page" aria-busy={loading}>
 		<h1 className="bookmark-page__title">Your Bookmarks</h1>
 		<Loader isLoading={loading} errorObj={error}/>
 		{!loading && !error && (
-			<PaginatedCards items={bookmarks?.map((b) => (
+			<PaginatedCards items={bookmarks?.map((b) => (<>
 				<PoiOverview
 					key={`${b.osm_type}${b.osm_id}`}
 					poi={b}
 					taxa={taxa}
 					headingTag="h2"
 					headerClassName="bookmark-page__poi-header" />
-			))} perPage={16} type="poi"/>
+				<button
+					className="bookmark-page__delete-btn"
+					onClick={() => deleteBookmark(b.id)}
+				>
+					<Icon src={closeSrc} alt="Delete bookmark" />
+				</button>
+			</>))} perPage={16} type="poi"/>
 		)}
 	</section>);
 }
